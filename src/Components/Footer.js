9c64dad219a20cf457974afb8ee37fb57d442921
Michelle Medina <medina.m1016@gmail.com>
2019-03-03 17:17:28 -0800

    insta logo

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -8,7 +8,7 @@ export class Footer extends Component {
                 <h1><a href="mailto:medina.m1016@gmail.com" target="_top"><b>Let's Connect!</b></a></h1><br />
                 <div id="footer-links">
                     <a href="https://github.com/mmedina3" rel="noopener noreferrer" target="_blank"><img src={require("../image/github.png")} alt="Github Link" /></a>
-                    <a href="https://www.linkedin.com/in/michellemed/" rel="noopener noreferrer" target="_blank"><img src={require("../image/linkedin.png")} alt="Linkedin Link" /></a>
+                    <a href="https://www.instagram.com/mmedina3/" rel="noopener noreferrer" target="_blank"><img src={require("../image/insta.png")} alt="Instagram Link" /></a>
                     <a href="https://twitter.com/mmed1na" rel="noopener noreferrer" target="_blank"><img src={require("../image/twitter.png")} alt="Twitter Link" /></a>
                 </div>
                 <br />
